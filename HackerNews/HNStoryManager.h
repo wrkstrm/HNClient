352003de88b22ce2d5ff87934b204764547b85c5
Cristian A Monterroza <cmonterroza@wrkstrm.me>
2014-11-19 00:16:33 -0800

    Create the story manager.

---
+++ HackerNews/HNStoryManager.h
@@ -0,0 +1,13 @@
+//
+//  HNStoryManager.h
+//  HackerNews
+//
+//  Created by Cristian Monterroza on 11/19/14.
+//  Copyright (c) 2014 wrkstrm. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface HNStoryManager : NSObject
+
+@end
---
+++ HackerNews/HNStoryManager.m
@@ -0,0 +1,13 @@
+//
+//  HNStoryManager.m
+//  HackerNews
+//
+//  Created by Cristian Monterroza on 11/19/14.
+//  Copyright (c) 2014 wrkstrm. All rights reserved.
+//
+
+#import "HNStoryManager.h"
+
+@implementation HNStoryManager
+
+@end
